52d7d8a706bd9dee0c516c2b84144a0b48bab9d8
Deleplanque Matheo <matheo_deleplanque@ens.univ-artois.fr>
2024-11-19 12:30:27 +0100

    :tada: Création du projet !

---
+++ resources/views/statiques/accueil.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang=fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @vite("resources/css/app.css")
+    <title>SubTrack - Accueil</title>
+</head>
+<body class="w-screen h-screen bg-gradient-to-tl from-blue-500 to-green-800 text-white flex flex-col items-center pt-24">
+    <h1 class="text-5xl font-bold">Manager vos abonnements comme si tout était en un !</h1>
+    <button class="w-1/12 h-11 rounded-3xl bg-white text-black font-bold mt-12">Démarrer</button>
+</body>
+</html>
